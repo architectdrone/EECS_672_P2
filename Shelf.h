63337a469deaa5728be14543b09a62a243c1f828
Owen Mellema <owenmllm@gmail.com>
2019-10-23 17:06:16 -0500

    Create Shelf

--- Shelf.h
+++ Shelf.h
@@ -1,17 +1,17 @@
-// TEMPLATE_Subclass.h
+// Shelf.h
 
-#ifndef TEMPLATE_SUBCLASS_H
-#define TEMPLATE_SUBCLASS_H
+#ifndef Shelf_H
+#define Shelf_H
 
 #include "ModelView.h"
 #include "ShaderIF.h"
 
-class TEMPLATE_Subclass : public ModelView
+class Shelf : public ModelView
 {
 public:
 	// As before: you will likely want to add parameters to the constructor
-	TEMPLATE_Subclass(ShaderIF* sIF);
-	virtual ~TEMPLATE_Subclass();
+	Shelf(ShaderIF* sIF);
+	virtual ~Shelf();
 
 	// xyzLimits: {mcXmin, mcXmax, mcYmin, mcYmax, mcZmin, mcZmax}
 	void getMCBoundingBox(double* xyzLimitsF) const;
